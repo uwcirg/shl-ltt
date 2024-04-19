dd3e4f9dd8d1d9a66222f6e82ca9408416bd030c
Daniel Lorigan <daniellrgn@gmail.com>
2024-04-19 12:37:07 -0700

    Clean up config.ts and update default.env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,58 +4,20 @@ import { toMilliseconds } from '$lib/util';
 
 export const API_BASE = import.meta.env.VITE_API_BASE;
 
-export const INTERMEDIATE_FHIR_SERVER_BASE = import.meta.env.VITE_INTERMEDIATE_FHIR_SERVER_BASE;
-
 export const OIDC_BASE = import.meta.env.VITE_OIDC_SERVER_BASE;
 export const CHECK_SESSION_IFRAME = import.meta.env.VITE_OIDC_CHECK_SESSION_IFRAME;
 export const LOGOUT_URL = import.meta.env.VITE_OIDC_LOGOUT_ENDPOINT;
 
 export const BACK_URL = import.meta.env.VITE_BACK_URL;
 
+export const FHIR_R4_EXTERNAL_ID_SYSTEM = import.meta.env.VITE_FHIR_R4_EXTERNAL_ID_SYSTEM;
+
 const timeout = (import.meta.env.VITE_INACTIVITY_TIMEOUT ?? "04:00:00").split(":").map((n) => Number(n));
 export const INACTIVITY_TIMEOUT = toMilliseconds(timeout[0] ?? 0, timeout[1] ?? 0, timeout[2] ?? 0);
 
-export const SOF_RESOURCES = [
-  'Patient',
-  'AllergyIntolerance',
-  // 'MedicationStatement', // Not in EPIC USCDI R4
-  'MedicationRequest',
-  'Medication', // can't search by patient; "Only an _ID search is allowed."
-  'Condition',
-  'Observation', // "Must have either code or category."
-  'Organization', // can't search by patient; "Only an _ID search is allowed."
-  'Immunization',
-  'Device',
-  // 'DeviceUseStatement', // Not in EPIC USCDI R4
-  'DiagnosticReport', // TODO change to subject
-  // 'ImagingStudy', // Not in EPIC USCDI R4
-  // 'Media', // Not in EPIC USCDI R4
-  'Practitioner', // can't search by patient; "Either name, family, or identifier is a required parameter."
-  'PractitionerRole',  // can't search by patient; "An identifier, practitioner, organization, location, or specialty parameter is required."
-  'Procedure', // TODO change to subject
-  // 'Specimen', // Not in EPIC USCDI R4
-];
-
 export const SOF_PATIENT_RESOURCES = [
   'Patient',
-  'DocumentReference',
-  // 'AllergyIntolerance',
-  // 'MedicationStatement', // Not in EPIC USCDI R4
-  // 'MedicationRequest',
-  // 'Medication', // can't search by patient; "Only an _ID search is allowed."
-  // 'Condition',
-  // 'Observation', // "Must have either code or category."
-  // 'Organization', // can't search by patient; "Only an _ID search is allowed."
-  // 'Immunization',
-  // 'Device',
-  // 'DeviceUseStatement', // Not in EPIC USCDI R4
-  // 'DiagnosticReport', // TODO change to subject
-  // 'ImagingStudy', // Not in EPIC USCDI R4
-  // 'Media', // Not in EPIC USCDI R4
-  // 'Practitioner', // can't search by patient; "Either name, family, or identifier is a required parameter."
-  // 'PractitionerRole',  // can't search by patient; "An identifier, practitioner, organization, location, or specialty parameter is required."
-  // 'Procedure', // TODO change to subject
-  // 'Specimen', // Not in EPIC USCDI R4
+  'DocumentReference'
 ];
 
 export const RESOURCE_SCOPE = SOF_PATIENT_RESOURCES.map(resourceType => `patient/${resourceType}.read`).join(" ");
@@ -64,20 +26,11 @@ const fullScope = `${keycloakScope} fhirUser ${RESOURCE_SCOPE}`;
 const SOF_REDIRECT_URI = `${window.location.origin}/share`;
 
 export const SOF_HOSTS = [
-  // {
-  //   id: "smit",
-  //   name: "SMART Health IT Demo",
-  //   iss: "https://launch.smarthealthit.org/v/r4/sim/WzMsIiIsIiIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMF0/fhir",
-  //   clientId: "<no client id>",
-  //   scope: fullScope,
-  //   redirect_uri: SOF_REDIRECT_URI,
-  //   note: "Credentials provided"
-  // },
   {
     id: "keycloak",
     name: "Let's Talk Tech Login",
-    iss: import.meta.env.VITE_SOF_ISS,//"https://fhir-auth.inform.dev.cirg.uw.edu/fhir",
-    clientId: import.meta.env.VITE_SOF_CLIENT_ID, // shl_creator
+    iss: import.meta.env.VITE_SOF_ISS,
+    clientId: import.meta.env.VITE_SOF_CLIENT_ID,
     scope: keycloakScope,
     redirect_uri: SOF_REDIRECT_URI,
     note: "Credentials provided"
@@ -88,15 +41,3 @@ export const VIEWER_BASE = new URL(
   (import.meta.env.VITE_VIEWER_BASE || `/view${dev ? '/index.html' : ''}`)+'#',
   window.location.href
 ).toString();
-export const PATIENT_IPS = {
-  'Dave deBronkart': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/11013/$summary'
-}
-export const EXAMPLE_IPS = {
-  'Maria SEATTLE Gravitate': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/14599/$summary',
-  'Peter Keith Jones': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/11013/$summary',
-  'Angela Roster': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/10965/$summary',
-  'Horace Skelly': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/11142/$summary',
-  'Anonymous': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/10999/$summary',
-  'Desiree': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/Pat1-System2/$summary'
-};
-export const IPS_DEFAULT = 'Maria SEATTLE Gravitate';
